c8922c17c0f191ab6b716bd71c2cd11a38b0a04b
Dan Coffey <danielpcoffey@gmail.com>
2014-07-23 10:17:24 -0400

    v1 working 2 buttons

--- rs232/ViewController.h
+++ rs232/ViewController.h
@@ -7,8 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "inc/RscMgr.h"
 
-@interface ViewController : UIViewController
+#define BUFFER_LEN 1024
+
+@interface ViewController : UIViewController <RscMgrDelegate> {
+    RscMgr *rscMgr;
+    UInt8 rxBuffer[BUFFER_LEN];
+    UInt8 txBuffer[BUFFER_LEN];
+}
+
+// set up my buttons
+
+@property (weak, nonatomic) IBOutlet UILabel *texty;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UILabel *connectLabel;
+
+- (IBAction)touchbutton1:(id)sender;
+- (IBAction)touchbutton2:(id)sender;
 
 
 @end
--- rs232/ViewController.m
+++ rs232/ViewController.m
@@ -9,20 +9,90 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-            
 
 @end
 
 @implementation ViewController
-            
+
+@synthesize texty;
+@synthesize connectLabel;
+@synthesize button1;
+@synthesize button2;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Create and initialize our RedparkSerialCable Manager for communicating
+    // with the serial cable.
+    rscMgr = [[RscMgr alloc] init];
+    
+    // Set this as our delegate so we can receive evevnts
+    [rscMgr setDelegate:self];
+    
+    // set defaults for analog_way
+    [rscMgr setBaud:115200];
+    [rscMgr setDataSize:(DataSizeType)8];
+//    [rscMgr	setParity:(ParityType)value];
+    [rscMgr setStopBits:(StopBitsType)1];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)touchbutton1:(id)sender {
+    NSString *s1 = @"0,1,1,1PRinp0,1PUscu0,1GCtak";
+    uint8_t *someInt = (uint8_t *)[s1 UTF8String];
+    [rscMgr write:someInt Length:[s1 length]];
+    texty.text = s1;
+    
+}
+- (IBAction)touchbutton2:(id)sender {
+    NSString *s1 = @"1,1,1,1PRinp1,1PUscu1,1GCtak";
+    uint8_t *someInt = (uint8_t *)[s1 UTF8String];
+    [rscMgr write:someInt Length:[s1 length]];
+    texty.text = s1;
+    
+}
+
+
+#pragma mark - RscMgrDelegate methods
+
+- (void) cableConnected:(NSString *)protocol {
+    connectLabel.text = @"Connected";
+
+    [rscMgr setBaud:115200];
+    [rscMgr open];
+}
+
+- (void) cableDisconnected {
+    connectLabel.text = @"Not Connected";
+    
+}
+
+- (void) portStatusChanged {
+    
+}
+
+- (void) readBytesAvailable:(UInt32)numBytes {
+}
+
+- (BOOL) rscMessageReceived:(UInt8 *)msg TotalLength:(int)len {
+    return FALSE;
+}
+
+- (void) didReceivePortConfig {
+}
+
 
 @end
